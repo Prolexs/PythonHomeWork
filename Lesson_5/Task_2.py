5fd04570b89e7a4cbc8a2b4b450447813e58c1ae
Алексей Старченко <Prolex>
2023-04-22 15:41:42 +0400

    задача решена

---
+++ Lesson_5/Task_2.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    if b == 0:
+        return a
+    elif a == 0:
+        return b
+    else:
+        return sum(a + 1, b - 1)
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+result = sum(a, b)
+
+print(f"{a} + {b} = {result}")
