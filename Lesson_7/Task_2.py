6d273263334231c41e9abf106736391ee9c27dad
Алексей Старченко <Prolex>
2023-05-19 19:28:30 +0400

    задача решена

---
+++ Lesson_7/Task_2.py
@@ -0,0 +1,15 @@
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            result = operation(i, j)
+            print(f'{result:5}', end=' ')
+        print()
+
+# Пример использования
+
+# Функция для вычисления произведения
+def multiply(x, y):
+    return x * y
+
+# Вывод таблицы произведений чисел от 1 до 6
+print_operation_table(multiply)
